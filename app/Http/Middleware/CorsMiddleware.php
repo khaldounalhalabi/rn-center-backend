3a57dce7e9a3a5364765329df5acc52591123cc9
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-03-21 01:43:04 +0300

    CORS Middleware more safe but need to more checking

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,10 +17,8 @@ class CorsMiddleware
     {
         //TODO::never forget to check on this
          return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', '*')
-        ->header('Access-Control-Allow-Credentials', true)
-        ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
-        ->header('Accept', 'application/json');
+        ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->header('Access-Control-Allow-Methods', 'GET, POST , DELETE , PUT')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
