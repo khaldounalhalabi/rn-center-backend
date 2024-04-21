962d3eff4295c881eef800dcec1dc8954322882c
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-04-22 02:24:54 +0300

    fixing update customer data

--- app/Http/Requests/AuthRequests/UpdateUserRequest.php
+++ app/Http/Requests/AuthRequests/UpdateUserRequest.php
@@ -3,8 +3,12 @@
 namespace App\Http\Requests\AuthRequests;
 
 use App\Enums\GenderEnum;
+use App\Models\User;
+use App\Rules\LanguageShape;
+use App\Rules\UniquePhoneNumber;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Propaganistas\LaravelPhone\Rules\Phone;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -29,13 +33,20 @@ class UpdateUserRequest extends FormRequest
             'first_name' => 'nullable|string|max:255|min:3',
             'middle_name' => 'nullable|string|max:255|min:3',
             'last_name' => 'nullable|string|max:255|min:3',
-            'mother_full_name' => 'nullable|string|min:3|max:255',
-            'phone_number' => 'nullable|min:10|max:11|unique:users,phone_number,' . auth()->user()->id,
-            'email' => 'nullable|email|min:3|max:255|unique:users,email,' . auth()->user()->id,
+            'phone_number' => 'array|nullable',
+            'phone_number.*' => ['nullable', 'string', 'unique:phone_numbers,phone', (new Phone())->country(['IQ']), new UniquePhoneNumber(auth()->user()->id)],
+            'email' => 'nullable|email|unique:users,email|min:3|max:255',
+            'password' => 'nullable|min:8|confirmed|max:255',
             'fcm_token' => 'nullable|string|min:3|max:1000',
             'gender' => 'nullable|string|' . Rule::in(GenderEnum::getAllValues()),
-            'city' => 'string|max:255|min:3|required',
-            'image' => 'image|max:50000|mimes:jpg,png|nullable'
+            'image' => 'image|max:50000|mimes:jpg,png|nullable',
+            'birth_date' => 'nullable|date|date_format:Y-m-d',
+            'address' => 'nullable|array',
+            'address.name' => ['nullable', 'string', new LanguageShape()],
+            'address.city_id' => ['nullable', 'exists:cities,id', 'integer'],
+            'address.lat' => ['nullable', 'string', 'nullable_without:address.map_iframe'],
+            'address.lng' => ['nullable', 'string', 'nullable_without:address.map_iframe'],
+            'address.map_iframe' => ['nullable', 'string', 'nullable_without:address.lat', 'nullable_without:address.lng']
         ];
     }
 }
