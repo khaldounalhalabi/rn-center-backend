1644c5b7d915e636cff26aa7f25c1d039237953a
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-05-15 01:20:27 +0300

    removing unused routes

--- routes/v1/api/admin.php
+++ routes/v1/api/admin.php
@@ -26,49 +26,35 @@ Route::controller(v1\ScheduleController::class)
     });
 
 Route::apiResource("/customers", v1\CustomerController::class)->names("customers");
-Route::get('/hospitals/all', [v1\HospitalController::class, 'getAll'])->name('hospital.all');
+
 Route::apiResource("/hospitals", v1\HospitalController::class)->names("hospitals");
+
 Route::apiResource("/phone-numbers", v1\PhoneNumberController::class)->names("phone.numbers");
+
 Route::apiResource("/available-departments", v1\AvailableDepartmentController::class)->names("available.departments");
+
 Route::apiResource("/specialities", v1\SpecialityController::class)->names("specialities");
+
 Route::apiResource("/addresses", v1\AddressController::class)->names("addresses");
+
 Route::apiResource('/cities', v1\CityController::class)->names('cities');
+
 Route::apiResource('/clinic-holidays', v1\ClinicHolidayController::class)->names('clinic.holidays');
 
-Route::post('/service-categories/export', [v1\ServiceCategoryController::class, 'export'])->name('service.categories.export');
-Route::post('/service-categories/import', [v1\ServiceCategoryController::class, 'import'])->name('service.categories.import');
-Route::get('/service-categories/get-import-example', [v1\ServiceCategoryController::class, 'getImportExample'])->name('service.categories.get.example');
 Route::apiResource('/service-categories', v1\ServiceCategoryController::class)->names('service.categories');
 
-Route::post('/services/export', [v1\ServiceController::class, 'export'])->name('services.export');
-Route::post('/services/import', [v1\ServiceController::class, 'import'])->name('services.import');
-Route::get('/services/get-import-example', [v1\ServiceController::class, 'getImportExample'])->name('services.get.example');
 Route::apiResource('/services', v1\ServiceController::class)->names('services');
 
-Route::post('/appointments/{appointmentId}/toggle-status', [v1\AppointmentController::class, 'toggleAppointmentStatus'])->name('appointments.status.toggle');
+
+Route::post('appointments/{appointmentId}/toggle-status', [v1\AppointmentController::class, 'toggleAppointmentStatus'])->name('appointments.status.toggle');
 Route::get('appointments/{appointmentId}/prescriptions/', [v1\PrescriptionController::class, 'getAppointmentPrescriptions'])->name('appointments.prescriptions');
 Route::get('appointment-logs/{appointmentLogId}', [v1\AppointmentLogController::class, 'show'])->name('appointment.log.show');
 Route::get('appointments/{appointmentId}/logs', [v1\AppointmentLogController::class, 'getAppointmentLogs'])->name('appointments.logs');
 Route::apiResource('/appointments', v1\AppointmentController::class)->names('appointments');
-Route::prefix('appointments')
-    ->name('appointments.')
-    ->controller(v1\AppointmentController::class)
-    ->group(function () {
-        Route::post('/export', 'export')->name('export');
-        Route::post('/import', 'import')->name('import');
-        Route::get('/get-import-example', 'getImportExample')->name('get.example');
-    });
 
-
-Route::post('/medicines/export', [v1\MedicineController::class, 'export'])->name('medicines.export');
-Route::post('/medicines/import', [v1\MedicineController::class, 'import'])->name('medicines.import');
-Route::get('/medicines/get-import-example', [v1\MedicineController::class, 'getImportExample'])->name('medicines.get.example');
 Route::apiResource('/medicines', v1\MedicineController::class)->names('medicines');
 
 Route::delete('/prescriptions/medicine-data/{medicineDataId}', [v1\PrescriptionController::class, 'removeMedicine'])->name('prescription.medicine.remove');
-Route::post('prescriptions/export', [v1\PrescriptionController::class, 'export'])->name('prescriptions.export');
-Route::post('prescriptions/import', [v1\PrescriptionController::class, 'import'])->name('prescriptions.import');
-Route::get('prescriptions/get-import-example', [v1\PrescriptionController::class, 'getImportExample'])->name('prescriptions.get.example');
 Route::apiResource('prescriptions', v1\PrescriptionController::class)->names('prescriptions');
 
 Route::apiResource('/blocked-items', v1\BlockedItemController::class)->names('blocked.items');
