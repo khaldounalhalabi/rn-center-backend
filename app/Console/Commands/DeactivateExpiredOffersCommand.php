aeb44df2aaa99cfe8dc3f2b93205e0635f73b6c3
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-05-31 03:24:57 +0300

    update DeactivateExpiredOffersCommand.php

--- app/Console/Commands/DeactivateExpiredOffersCommand.php
+++ app/Console/Commands/DeactivateExpiredOffersCommand.php
@@ -2,21 +2,19 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\Offer;
+use Illuminate\Console\Command;
 
 class DeactivateExpiredOffersCommand extends Command
 {
     /**
      * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'app:deactivate-expired-offers-command';
 
     /**
      * The console command description.
-     *
      * @var string
      */
     protected $description = 'Command description';
@@ -26,9 +24,12 @@ class DeactivateExpiredOffersCommand extends Command
      */
     public function handle()
     {
-        Offer::where('end_at' , '<' , now()->format('Y-m-d'))
+        $this->info("Command Started");
+        Offer::where('end_at', '<', now()->format('Y-m-d'))
             ->update([
                 'is_active' => false
             ]);
+
+        $this->info("Command Ended");
     }
 }
