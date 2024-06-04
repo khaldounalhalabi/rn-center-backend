d467fd3a47ed5a68967220b199e3047d82112e73
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-06-05 00:00:49 +0300

    fixes

--- database/factories/EnquiryFactory.php
+++ database/factories/EnquiryFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends Factory
@@ -12,15 +11,14 @@ class EnquiryFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         $bool = fake()->boolean;
         return [
-            'name' => $bool ? "khaldoun" : "mustafa",
-            'email' => $bool ? "khaldounalhalabi42@gmail.com" : "asasimr55@gmail.com",
+            'name'    => $bool ? "khaldoun" : "mustafa",
+            'email'   => $bool ? "khaldoun1222@hotmail.com" : "asasimr55@gmail.com",
             'message' => fake()->text(),
         ];
     }
