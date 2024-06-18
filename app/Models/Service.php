b0bd54898fb2f5aec75f1975f37819195c1219bf
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-06-18 15:11:53 +0300

    fixes

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -111,19 +111,19 @@ class Service extends Model implements HasMedia
 
     public function canShow(): bool
     {
-        return $this->clinic_id == auth()?->user()?->clinic->id
+        return $this->clinic_id == auth()?->user()?->clinic?->id
             || auth()->user()?->isAdmin();
     }
 
     public function canEdit(): bool
     {
-        return $this->clinic_id == auth()?->user()?->clinic->id
+        return $this->clinic_id == auth()?->user()?->clinic?->id
             || auth()->user()?->isAdmin();
     }
 
     public function canDelete(): bool
     {
-        return $this->clinic_id == auth()?->user()?->clinic->id
+        return $this->clinic_id == auth()?->user()?->clinic?->id
             || auth()->user()?->isAdmin();
     }
 }
