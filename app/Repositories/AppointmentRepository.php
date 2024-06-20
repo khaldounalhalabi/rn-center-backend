5fee8f37e7bc542329580fc1a40a204e4e0b81b2
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-06-21 02:59:15 +0300

    fixes

--- app/Repositories/AppointmentRepository.php
+++ app/Repositories/AppointmentRepository.php
@@ -4,9 +4,11 @@ namespace App\Repositories;
 
 use App\Enums\AppointmentStatusEnum;
 use App\Models\Appointment;
+use App\Models\AppointmentLog;
 use App\Repositories\Contracts\BaseRepository;
 use Carbon\Carbon;
 use DateTime;
+use Illuminate\Support\Collection;
 
 /**
  * @extends  BaseRepository<Appointment>
@@ -55,17 +57,31 @@ class AppointmentRepository extends BaseRepository
 
     /**
      * @param                        $clinicId
+     * @param                        $appointmentSequence
      * @param string|Carbon|DateTime $date
      * @param array                  $data
      * @return bool|int
      */
-    public function updatePreviousCheckinClinicAppointments($clinicId , $appointmentSequence, string|Carbon|DateTime $date, array $data): bool|int
+    public function updatePreviousCheckinClinicAppointments($clinicId, $appointmentSequence, string|Carbon|DateTime $date, array $data): bool|int
     {
         return $this->globalQuery()
             ->where('clinic_id', $clinicId)
             ->where('date', $date)
             ->where('status', AppointmentStatusEnum::CHECKIN->value)
-            ->where('appointment_sequence' , '<' , $appointmentSequence)
+            ->where('appointment_sequence', '<', $appointmentSequence)
+            ->chunk(5, function (Collection $appointments) use ($data) {
+                foreach ($appointments as $appointment) {
+                    AppointmentLog::create([
+                        'cancellation_reason' => $data['cancellation_reason'] ?? "",
+                        'status'              => $data['status'] ?? $appointment->status,
+                        'happen_in'           => now(),
+                        'appointment_id'      => $appointment->id,
+                        'actor_id'            => auth()->user()?->id,
+                        'affected_id'         => $data['customer_id'] ?? $appointment->customer_id,
+                        'event'               => "appointment status has been changed to {$data['status']} in " . now()->format('Y-m-d H:i:s') . " By " . auth()->user()->full_name->en
+                    ]);
+                }
+            })
             ->update($data);
     }
 }
