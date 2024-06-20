c2be8d8fb0fb9270e2ec66d694bac126971b35fc
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-06-21 01:49:00 +0300

    fix reset password email

--- resources/views/emails/verification-code.blade.php
+++ resources/views/emails/verification-code.blade.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
     <meta charset="UTF-8">
@@ -10,10 +11,15 @@
     <title></title>
     <!--[if (mso 16)]>
     <style type="text/css">
-        a {text-decoration: none;}
+        a {
+            text-decoration: none;
+        }
     </style>
     <![endif]-->
-    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <!--[if gte mso 9]>
+    <style>sup {
+        font-size: 100% !important;
+    }</style><![endif]-->
     <!--[if gte mso 9]>
     <xml>
         <o:OfficeDocumentSettings>
@@ -39,7 +45,8 @@
                     <tbody>
                     <tr>
                         <td class="esd-stripe" align="center">
-                            <table class="es-content-body" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+                            <table class="es-content-body" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff"
+                                   align="center">
                                 <tbody>
                                 <tr>
                                     <td class="es-p20t es-p20r es-p20l esd-structure" align="left">
@@ -50,31 +57,51 @@
                                                     <table width="100%" cellspacing="0" cellpadding="0">
                                                         <tbody>
                                                         <tr>
-                                                            <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank"><img class="adapt-img" src="https://ehughke.stripocdn.email/content/guids/CABINET_1033a9df9e92928a3edd19bfd6d14b975b96c4a4ec2e160e1b9b18b8c488608f/images/test.png" alt style="display: block;" width="294"></a></td>
+                                                            <td align="center" class="esd-block-image"
+                                                                style="font-size: 0px;"><a target="_blank"><img
+                                                                        class="adapt-img"
+                                                                        src="https://ehughke.stripocdn.email/content/guids/CABINET_1033a9df9e92928a3edd19bfd6d14b975b96c4a4ec2e160e1b9b18b8c488608f/images/test.png"
+                                                                        alt style="display: block;" width="294"></a>
+                                                            </td>
                                                         </tr>
                                                         <tr>
                                                             <td align="center" class="esd-block-text es-p5">
-                                                                <p style="color: #32ade6; font-size: 32px; font-family: -apple-system, blinkmacsystemfont, 'segoe ui', roboto, helvetica, arial, sans-serif, 'apple color emoji', 'segoe ui emoji', 'segoe ui symbol';"><strong>You Are Almost There!</strong></p>
+                                                                <p style="color: #32ade6; font-size: 32px; font-family: -apple-system, blinkmacsystemfont, 'segoe ui', roboto, helvetica, arial, sans-serif, 'apple color emoji', 'segoe ui emoji', 'segoe ui symbol';">
+                                                                    <strong>You Are Almost There!</strong></p>
                                                             </td>
                                                         </tr>
                                                         <tr>
                                                             <td align="center" class="esd-block-text">
-                                                                <p style="font-size: 18px; color: #8e8e93;"><strong>Only one step left to become a part of Cohort Family.</strong></p>
+                                                                <p style="font-size: 18px; color: #8e8e93;"><strong>Only
+                                                                        one step left to become a part of Cohort
+                                                                        Family.</strong></p>
                                                             </td>
                                                         </tr>
                                                         <tr>
                                                             <td align="center" class="esd-block-text es-p15t es-p15b">
-                                                                <p style="color: #32ade6; font-size: 72px; line-height: 120%;"><span style="background-color: #ebf7fc;">{{$code}}</span></p>
+                                                                <p style="color: #32ade6; font-size: 72px; line-height: 120%;">
+                                                                    <span
+                                                                        style="background-color: #ebf7fc;">{{$code}}</span>
+                                                                </p>
                                                             </td>
                                                         </tr>
                                                         <tr>
                                                             <td align="center" class="esd-block-text">
-                                                                <p style="font-size: 18px; color: #8e8e93;"><strong>This code is valid for the next 15 minutes.</strong></p>
+                                                                <p style="font-size: 18px; color: #8e8e93;"><strong>This
+                                                                        code is valid for the next 15 minutes.</strong>
+                                                                </p>
                                                             </td>
                                                         </tr>
                                                         <tr>
-                                                            <td align="center" class="esd-block-text es-p30t es-p30b"><a class="esd-anchor" name="mailto:support@cohort.com"></a>
-                                                                <p style="font-size: 18px; color: #8e8e93;"><strong><span>If you didn't request this email or if you think something is wrong, feel free to contact our support team at <span style="color:#32ADE6;">support@pom.com</span><span style="color:#8e8e93;">.</span></span></strong><strong><span> We're here to help!</span></strong></p>
+                                                            <td align="center" class="esd-block-text es-p30t es-p30b"><a
+                                                                    class="esd-anchor"
+                                                                    name="mailto:support@cohort.com"></a>
+                                                                <p style="font-size: 18px; color: #8e8e93;">
+                                                                    <strong><span>If you didn't request this email or if you think something is wrong, feel free to contact our support team at <span
+                                                                                style="color:#32ADE6;"><a
+                                                                                    href="mailto:support@pom.com">support@pom.com</a></span><span
+                                                                                style="color:#8e8e93;">.</span></span></strong><strong><span> We're here to help!</span></strong>
+                                                                </p>
                                                             </td>
                                                         </tr>
                                                         </tbody>
