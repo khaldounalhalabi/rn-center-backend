6e51cb982b4f02c476e01fd27cf951b90c9c787c
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-07-04 00:19:08 +0300

    fix system offers relations

--- app/Http/Controllers/API/v1/SystemOfferController.php
+++ app/Http/Controllers/API/v1/SystemOfferController.php
@@ -18,7 +18,7 @@ class SystemOfferController extends ApiController
         $this->systemOfferService = SystemOfferService::make();
         // place the relations you want to return them within the response
         $this->relations = ['media', 'clinics'];
-        $this->indexRelations = ['media'];
+        $this->indexRelations = [];
     }
 
     public function index()
