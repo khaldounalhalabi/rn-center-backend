d8ed54c6ed6f7f1f9f22e04abe258faf63bfd4dd
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-08-17 19:46:13 +0300

    fix customer middleware

--- app/Http/Middleware/MustVerifyPhone.php
+++ app/Http/Middleware/MustVerifyPhone.php
@@ -19,6 +19,10 @@ class MustVerifyPhone
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!auth()->user()?->isCustomer()) {
+            return $this->apiResponse(null, ApiController::STATUS_UNAUTHORIZED, __('site.unauthorized_user'));
+        }
+
         if (!auth()->user()?->hasVerifiedPhoneNumber()) {
             return $this->apiResponse(null, ApiController::STATUS_UNVERIFIED_PHONE_NUMBER, __('site.un_verified_phone'));
         }
