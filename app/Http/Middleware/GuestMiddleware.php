4ea6b1da29bb802c598e21c2a4306438a8923b06
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-08-25 23:14:55 +0300

    1 - fixing factories 2 - collect monthly subscription amount from the clinic

---
+++ app/Http/Middleware/GuestMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GuestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->user()) {
+            $request->headers->add([
+                'Guest' => true,
+            ]);
+        }
+        return $next($request);
+    }
+}
