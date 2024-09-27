a9e6ecd8045cb46eb22b059d8b63a36e20325790
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-09-28 01:16:35 +0300

    Revert "1- add deploy frontend controller"
    This reverts commit 45df0aca0453ec1dddb59e64b5aa5d5a48ae2535.

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/deploy', [\App\Http\Controllers\DeployController::class, 'frontend']);
+Route::get('/deploy', [\App\Http\Controllers\DeployController::class, 'frontend']);
 
