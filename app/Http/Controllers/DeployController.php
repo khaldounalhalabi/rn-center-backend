5908468bcdd0043f9aa5efae676f53f5323dbfe2
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-09-28 01:07:41 +0300

    1- add deploy frontend controller

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -7,7 +7,18 @@ class DeployController extends Controller
     public function frontend()
     {
         try {
-            $output = shell_exec('/home/pom/deploy-main.sh');
+            $output = shell_exec('
+                export PATH=/opt/cpanel/ea-nodejs18/bin/:$PATH
+                && cd /home/pom/public_html/pom-front/
+                && pm2 kill
+                && rm -r -f .next
+                && git reset --hard
+                && git clean -df
+                && git pull origin
+                && npm i
+                && npm run build
+                && pm2 start ./ecosystem.config.js
+            ');
             return response()->json([
                 'message' => 'success',
                 'output'  => $output
