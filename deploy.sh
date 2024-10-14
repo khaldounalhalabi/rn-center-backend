77c05e6c5c6cd00d41e563458b9bd9a4274fe578
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-10-15 01:32:16 +0300

    1- add worker.conf

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+echo "Heading to project directory"
+
+echo "Pulling ..."
+git reset --hard
+git clean -df
+git pull origin
+
+echo "Installing composer packages"
+composer install
+
+echo "running migrations"
+php artisan migrate
+
+echo "Cleaning cache"
+php artisan optimize:clear
+
+echo "Restarting jobs"
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start "laravel-worker:laravel-worker-dev"
+supervisorctl restart "laravel-worker:laravel-worker-dev"
