ab1b766e5cbaa3748c1f6414af27311211877783
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-10-21 23:57:42 +0300

    1- fix AppointmentFactory.php

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -30,18 +30,18 @@ class AppointmentFactory extends Factory
     {
         $type = fake()->randomElement(AppointmentTypeEnum::getAllValues());
         return [
-            'customer_id'          => Customer::inRandomOrder()->first()?->id,
-            'clinic_id'            => Clinic::inRandomOrder()->first()?->id,
-            'note'                 => fake()->text(),
-            'service_id'           => Service::inRandomOrder()->first()?->id,
-            'extra_fees'           => fake()->randomFloat(1, 2000),
-            'total_cost'           => fake()->randomFloat(2, 0, 1000),
-            'type'                 => $type,
-            'date'                 => fake()->dateTimeBetween('-5 days', '+20 days'),
-            'status'               => $type == AppointmentTypeEnum::MANUAL->value
+            'customer_id' => Customer::inRandomOrder()->first()?->id ?? Customer::factory()->create()?->id,
+            'clinic_id' => Clinic::inRandomOrder()->first()?->id ?? Clinic::factory()->create()?->id,
+            'note' => fake()->text(),
+            'service_id' => Service::inRandomOrder()->first()?->id ?? Service::factory()->create()?->id,
+            'extra_fees' => fake()->randomFloat(1, 2000),
+            'total_cost' => fake()->randomFloat(2, 0, 1000),
+            'type' => $type,
+            'date' => fake()->dateTimeBetween('-5 days', '+20 days'),
+            'status' => $type == AppointmentTypeEnum::MANUAL->value
                 ? AppointmentStatusEnum::BOOKED->value
                 : AppointmentStatusEnum::PENDING->value,
-            'device_type'          => fake()->word(),
+            'device_type' => fake()->word(),
             'appointment_sequence' => fake()->numberBetween(1, 10),
         ];
     }
@@ -56,10 +56,10 @@ class AppointmentFactory extends Factory
         return $this->afterCreating(function (Appointment $app) {
             AppointmentLog::create([
                 'appointment_id' => $app->id,
-                'status'         => $app->status,
-                'actor_id'       => $app->clinic_id,
-                'affected_id'    => $app->customer_id,
-                'happen_in'      => now(),
+                'status' => $app->status,
+                'actor_id' => $app->clinic_id,
+                'affected_id' => $app->customer_id,
+                'happen_in' => now(),
             ]);
         });
     }
