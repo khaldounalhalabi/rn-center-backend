5ed63a17a789a7be3716d062b7d26016829de4d5
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-10-24 22:51:29 +0300

    1- fix:create clinic endpoint

--- app/Services/ClinicService.php
+++ app/Services/ClinicService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Enums\ClinicStatusEnum;
 use App\Enums\RolesPermissionEnum;
-use App\Models\Appointment;
 use App\Models\Clinic;
 use App\Models\User;
 use App\Repositories\AddressRepository;
@@ -14,8 +13,8 @@ use App\Repositories\PhoneNumberRepository;
 use App\Repositories\UserRepository;
 use App\Services\Contracts\BaseService;
 use App\Traits\Makable;
-use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends BaseService<Clinic>
@@ -52,43 +51,51 @@ class ClinicService extends BaseService
      */
     public function store(array $data, array $relationships = [], array $countable = []): ?Clinic
     {
-        if (!isset($data['user'])
-            || !isset($data['address'])
-            || !isset($data['speciality_ids'])
-            || !isset($data['phone_numbers'])
-        ) {
+        try {
+            DB::beginTransaction();
+            if (!isset($data['user'])
+                || !isset($data['address'])
+                || !isset($data['speciality_ids'])
+                || !isset($data['phone_numbers'])
+            ) {
+                DB::commit();
+                return null;
+            }
+
+            /** @var User $user */
+            $user = $this->userRepository->create($data['user']);
+            $user->assignRole(RolesPermissionEnum::DOCTOR['role']);
+            $data['user_id'] = $user->id;
+
+            /** @var Clinic $clinic */
+            $clinic = $this->repository->create($data);
+
+            $clinic->specialities()->sync($data['speciality_ids']);
+
+            $data['address']['addressable_id'] = $user->id;
+            $data['address']['addressable_type'] = User::class;
+
+            $this->addressRepository->create($data['address']);
+
+            $this->phoneNumberRepository->insert($data['phone_numbers'], User::class, $user->id);
+
+            $this->scheduleService->setDefaultClinicSchedule($clinic);
+
+            $this->clinicSubscriptionService->store([
+                'clinic_id' => $clinic->id,
+                'subscription_id' => $data['subscription_id'],
+                'type' => $data['subscription_type'],
+                'deduction_cost' => $data['subscription_deduction_cost'] ?? 0,
+            ]);
+            DB::commit();
+            return $clinic->refresh()
+                ->load($relationships)
+                ->loadCount($countable);
+        } catch (\Exception $exception) {
+            logger()->info($exception->getMessage());
+            DB::rollBack();
             return null;
         }
-
-        /** @var User $user */
-        $user = $this->userRepository->create($data['user']);
-        $user->assignRole(RolesPermissionEnum::DOCTOR['role']);
-        $data['user_id'] = $user->id;
-
-        /** @var Clinic $clinic */
-        $clinic = $this->repository->create($data);
-
-        $clinic->specialities()->sync($data['speciality_ids']);
-
-        $data['address']['addressable_id'] = $user->id;
-        $data['address']['addressable_type'] = User::class;
-
-        $this->addressRepository->create($data['address']);
-
-        $this->phoneNumberRepository->insert($data['phone_numbers'], User::class, $user->id);
-
-        $this->scheduleService->setDefaultClinicSchedule($clinic);
-
-        $this->clinicSubscriptionService->store([
-            'clinic_id'       => $clinic->id,
-            'subscription_id' => $data['subscription_id'],
-            'type'            => $data['subscription_type'],
-            'deduction_cost'  => $data['subscription_deduction_cost'] ?? 0,
-        ]);
-
-        return $clinic->refresh()
-            ->load($relationships)
-            ->loadCount($countable);
     }
 
     /**
@@ -100,46 +107,55 @@ class ClinicService extends BaseService
      */
     public function update(array $data, $id, array $relationships = [], array $countable = []): ?Clinic
     {
-        $clinic = $this->repository->find($id);
+        try {
+            DB::beginTransaction();
+            $clinic = $this->repository->find($id);
 
-        if (!$clinic) {
+            if (!$clinic) {
+                return null;
+            }
+
+            if (!$clinic->canUpdate()) {
+                DB::commit();
+                return null;
+            }
+
+            $clinic = $this->repository->update($data, $clinic);
+
+
+            $user = $clinic->user;
+
+            if (isset($data['user'])) {
+                if (isset($data['password']) && $data['password'] == "") {
+                    unset($data['password']);
+                }
+                $this->userRepository->update($data['user'], $clinic->user_id);
+            }
+
+            if (isset($data['address'])) {
+                if ($user->address) {
+                    $user->address->update($data['address']);
+                } else {
+                    $user->address()->create($data['address']);
+                }
+            }
+
+            if (isset($data['speciality_ids'])) {
+                $clinic->specialities()->sync($data['speciality_ids']);
+            }
+
+            if (isset($data['phone_numbers'])) {
+                $user->phones()->delete();
+                $this->phoneNumberRepository->insert($data['phone_numbers'], User::class, $user->id);
+            }
+
+            DB::commit();
+            return $clinic->load($relationships)->loadCount($countable);
+        } catch (\Exception $exception) {
+            logger()->info($exception->getMessage());
+            DB::rollBack();
             return null;
         }
-
-        if (!$clinic->canUpdate()) {
-            return null;
-        }
-
-        $clinic = $this->repository->update($data, $clinic);
-
-
-        $user = $clinic->user;
-
-        if (isset($data['user'])) {
-            if (isset($data['password']) && $data['password'] == "") {
-                unset($data['password']);
-            }
-            $this->userRepository->update($data['user'], $clinic->user_id);
-        }
-
-        if (isset($data['address'])) {
-            if ($user->address) {
-                $user->address->update($data['address']);
-            } else {
-                $user->address()->create($data['address']);
-            }
-        }
-
-        if (isset($data['speciality_ids'])) {
-            $clinic->specialities()->sync($data['speciality_ids']);
-        }
-
-        if (isset($data['phone_numbers'])) {
-            $user->phones()->delete();
-            $this->phoneNumberRepository->insert($data['phone_numbers'], User::class, $user->id);
-        }
-
-        return $clinic->load($relationships)->loadCount($countable);
     }
 
     /**
@@ -152,23 +168,23 @@ class ClinicService extends BaseService
 
         if (!$clinic) {
             return [
-                'booked_times'    => [],
+                'booked_times' => [],
                 'clinic_schedule' => [],
                 'clinic_holidays' => [],
             ];
         }
 
         $bookedTimes = AppointmentRepository::make()->getByClinicDayRange($clinic)
-        ->groupBy('date')
-        ->map(function (Collection $appointments) {
-            return $appointments->count();
-        });
+            ->groupBy('date')
+            ->map(function (Collection $appointments) {
+                return $appointments->count();
+            });
 
         $schedules = $clinic->schedules->groupBy('day_of_week');
         $holidays = $clinic->validHolidays;
 
         return [
-            'booked_times'    => $bookedTimes,
+            'booked_times' => $bookedTimes,
             'clinic_schedule' => $schedules,
             'clinic_holidays' => $holidays,
         ];
