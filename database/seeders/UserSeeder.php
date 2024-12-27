1d48fd3b41bfd871b2df09d2bf63ef12c8b6a969
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-12-27 19:41:28 +0300

    Revert "Reapply "1- fix:create clinic endpoint""
    This reverts commit c08eba389f053c15e6167a1bdd7591480b93386e.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,93 +21,93 @@ class UserSeeder extends Seeder
     {
         User::factory()
             ->withPhoneNumbers()
-            ->withAddress()->create(['email' => 'admin@pom.com', 'password' => 'BvgeGL9KDjAz3S'])
+            ->withAddress()->create(['email' => 'admin@pom.com', 'password' => '123456789'])
             ->assignRole(RolesPermissionEnum::ADMIN['role']);
-//
-//        $user1 = User::factory()
-//            ->withPhoneNumbers()
-//            ->withAddress()->create([
-//                'email'    => 'khaldounalhalabi42@gmail.com',
-//                'password' => '123456789',
-//            ])->assignRole(RolesPermissionEnum::DOCTOR['role']);
-//
-//        $clinic = Clinic::factory()->create([
-//            'name'    => new Translatable(['en' => 'Almahaba', 'ar' => 'عيادة المحبة']),
-//            'user_id' => $user1->id,
-//        ]);
-//
-//        ClinicEmployee::factory()
-//            ->create([
-//                'user_id'   => User::factory()
-//                    ->create([
-//                        'email'       => 'khaldoun1222@hotmail.com',
-//                        'password'    => '123456789',
-//                        'first_name'  => 'staff',
-//                        'middle_name' => 'staff',
-//                        'last_name'   => 'staff',
-//                        'is_blocked'  => false,
-//                        'is_archived' => false,
-//                    ])->assignRole(RolesPermissionEnum::CLINIC_EMPLOYEE['role'])->id,
-//                'clinic_id' => $clinic->id
-//            ]);
-//
-//        ClinicSubscription::create([
-//            'start_time'      => now()->subDay(),
-//            'end_time'        => now()->addYear(),
-//            'clinic_id'       => $clinic->id,
-//            'status'          => SubscriptionStatusEnum::ACTIVE->value,
-//            'deduction_cost'  => 10,
-//            'subscription_id' => 2,
-//            'type'            => SubscriptionTypeEnum::BOOKING_COST_BASED->value,
-//        ]);
-//
-//        $user1 = User::factory()
-//            ->withPhoneNumbers()
-//            ->withAddress()->create([
-//                'email'    => 'asasimr55@gmail.com',
-//                'password' => '123456789',
-//            ])->assignRole(RolesPermissionEnum::DOCTOR['role']);
-//
-//        $clinic = Clinic::factory()->create([
-//            'name'    => new Translatable(['en' => 'POM', 'ar' => 'POM']),
-//            'user_id' => $user1->id,
-//        ]);
-//
-//        ClinicEmployee::factory()
-//            ->create([
-//                'user_id'   => User::factory()
-//                    ->create([
-//                        'email'       => 'asasimr55@staff.com',
-//                        'password'    => '123456789',
-//                        'first_name'  => 'staff',
-//                        'middle_name' => 'staff',
-//                        'last_name'   => 'staff',
-//                        'is_blocked'  => false,
-//                        'is_archived' => false,
-//                    ])->assignRole(RolesPermissionEnum::CLINIC_EMPLOYEE['role'])->id,
-//                'clinic_id' => $clinic->id
-//            ]);
-//
-//        ClinicSubscription::create([
-//            'start_time'      => now()->subDay(),
-//            'end_time'        => now()->addYear(),
-//            'clinic_id'       => $clinic->id,
-//            'status'          => SubscriptionStatusEnum::ACTIVE->value,
-//            'deduction_cost'  => 10,
-//            'subscription_id' => 2,
-//            'type'            => SubscriptionTypeEnum::BOOKING_COST_BASED->value,
-//        ]);
-//
-//        User::factory(2)
-//            ->withPhoneNumbers()
-//            ->withAddress()
-//            ->clinic()
-//            ->create();
-//
-//        User::factory(2)
-//            ->withPhoneNumbers()
-//            ->withAddress()
-//            ->customer()
-//            ->create();
+
+        $user1 = User::factory()
+            ->withPhoneNumbers()
+            ->withAddress()->create([
+                'email'    => 'khaldounalhalabi42@gmail.com',
+                'password' => '123456789',
+            ])->assignRole(RolesPermissionEnum::DOCTOR['role']);
+
+        $clinic = Clinic::factory()->create([
+            'name'    => new Translatable(['en' => 'Almahaba', 'ar' => 'عيادة المحبة']),
+            'user_id' => $user1->id,
+        ]);
+
+        ClinicEmployee::factory()
+            ->create([
+                'user_id'   => User::factory()
+                    ->create([
+                        'email'       => 'khaldoun1222@hotmail.com',
+                        'password'    => '123456789',
+                        'first_name'  => 'staff',
+                        'middle_name' => 'staff',
+                        'last_name'   => 'staff',
+                        'is_blocked'  => false,
+                        'is_archived' => false,
+                    ])->assignRole(RolesPermissionEnum::CLINIC_EMPLOYEE['role'])->id,
+                'clinic_id' => $clinic->id
+            ]);
+
+        ClinicSubscription::create([
+            'start_time'      => now()->subDay(),
+            'end_time'        => now()->addYear(),
+            'clinic_id'       => $clinic->id,
+            'status'          => SubscriptionStatusEnum::ACTIVE->value,
+            'deduction_cost'  => 10,
+            'subscription_id' => 2,
+            'type'            => SubscriptionTypeEnum::BOOKING_COST_BASED->value,
+        ]);
+
+        $user1 = User::factory()
+            ->withPhoneNumbers()
+            ->withAddress()->create([
+                'email'    => 'asasimr55@gmail.com',
+                'password' => '123456789',
+            ])->assignRole(RolesPermissionEnum::DOCTOR['role']);
+
+        $clinic = Clinic::factory()->create([
+            'name'    => new Translatable(['en' => 'POM', 'ar' => 'POM']),
+            'user_id' => $user1->id,
+        ]);
+
+        ClinicEmployee::factory()
+            ->create([
+                'user_id'   => User::factory()
+                    ->create([
+                        'email'       => 'asasimr55@staff.com',
+                        'password'    => '123456789',
+                        'first_name'  => 'staff',
+                        'middle_name' => 'staff',
+                        'last_name'   => 'staff',
+                        'is_blocked'  => false,
+                        'is_archived' => false,
+                    ])->assignRole(RolesPermissionEnum::CLINIC_EMPLOYEE['role'])->id,
+                'clinic_id' => $clinic->id
+            ]);
+
+        ClinicSubscription::create([
+            'start_time'      => now()->subDay(),
+            'end_time'        => now()->addYear(),
+            'clinic_id'       => $clinic->id,
+            'status'          => SubscriptionStatusEnum::ACTIVE->value,
+            'deduction_cost'  => 10,
+            'subscription_id' => 2,
+            'type'            => SubscriptionTypeEnum::BOOKING_COST_BASED->value,
+        ]);
+
+        User::factory(2)
+            ->withPhoneNumbers()
+            ->withAddress()
+            ->clinic()
+            ->create();
+
+        User::factory(2)
+            ->withPhoneNumbers()
+            ->withAddress()
+            ->customer()
+            ->create();
     }
 }
