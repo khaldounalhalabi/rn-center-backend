5a31c92359f33c3792998329ed3596707bf0c051
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-26 00:05:55 +0300

    Remove Telescope package and related configurations
    Deleted Telescope service provider, configuration files, database migrations, and assets. This cleanup eliminates unnecessary overhead and simplifies maintenance.

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         \App\Providers\CarbonServiceProvider::class ,
         \App\Providers\RequestServiceProvider::class,
         \App\Providers\ObserverServiceProvider::class,
